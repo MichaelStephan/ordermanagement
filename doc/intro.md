508a6a679074ffe80ac5a5eada40f36cd6c1e703
Michael Stephan <michael.stephan@sap.com>
2018-03-07 17:53:51 +0100

    initial upload

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ordermanagement
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ordermanagement/core_test.clj
@@ -0,0 +1,7 @@
+(ns ordermanagement.core-test
+  (:require [clojure.test :refer :all]
+            [ordermanagement.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
